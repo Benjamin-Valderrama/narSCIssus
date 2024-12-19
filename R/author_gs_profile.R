914db29ad732c7af3d31b4d9e1f6eb4054a5f3e2
Benjamin-Valderrama <bvalderramabobadilla@gmail.com>
2024-12-19 00:26:04 -0300

    change magrittr's to base r's pipes

--- R/author_gs_profile.R
+++ R/author_gs_profile.R
@@ -9,9 +9,10 @@ author_gs_profile <- function(search_term){
                           replacement = "")
 
         author_gs_profile <- scholar::get_scholar_id(last_name = last_name,
-                                                     first_name = first_name) %>%
+                                                     first_name = first_name) |>
                 scholar::get_profile()
 
         return(author_gs_profile)
 
 }
+|>

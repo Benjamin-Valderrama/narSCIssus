3ddbe2c126a532ef66709b5c6eababafa1050ca5
Benjamin-Valderrama <bvalderramabobadilla@gmail.com>
2024-12-20 17:11:13 -0300

    incorporate functions to format text

--- R/businesscard_text.R
+++ R/businesscard_text.R
@@ -2,31 +2,17 @@ businesscard_text <- function(search_term){
 
         gs <- author_gs_profile(search_term)
 
-
-        # Format fields for plotting
-        formatted_fields <- character()
-
-        for (i in 1:length(gs$fields)){
-                if(i %% 2 != 0){
-                        field <- paste0(gs$fields[i], "   ")
-                        formatted_fields <- c(formatted_fields, field)}
-                else {
-                        field <- paste0(gs$fields[i], "\n")
-                        formatted_fields <- c(formatted_fields, field)
-                }
-        }
-
-        formatted_fields <- paste(formatted_fields, collapse = "")
-
+        formatted_fields <- format_fields(gs$fields)
+        formatted_affiliations <- format_affiliations(gs$affiliation)
 
         # make the data.frame with author information
         card_df <- data.frame(title = c("Full Name",
                                         "Affiliation",
-                                        "Total cites",
+                                        "Total citations",
                                         "H-index",
                                         "Fields"),
                               value = c(gs$name,
-                                        gs$affiliation,
+                                        formatted_affiliations,
                                         gs$total_cites,
                                         gs$h_index,
                                         formatted_fields),
